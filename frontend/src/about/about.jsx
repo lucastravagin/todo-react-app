858bb8c6697733957bf198a40057d7a66a7e53d6
Lucas <lucas_travagin1112@hotmail.com>
2018-08-14 16:17:24 -0300

    Concluindo componentes

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="Nós"></PageHeader>
+        <h2>Nossa História</h2>
+        <h3>Missão</h3>
+        <h2>Visão</h2>
+        <h2>Valores</h2>
     </div>    
 )
---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Grid from  '../template/grid'
+
+export default props => (
+  <div role="form" className="todoForm">
+    <div className="row">
+      <Grid cols="12 9 10">
+        <input id="description" className="form-control" placeholder="Adicione uma tarefa"></input>
+      </Grid>
+      <Grid cols="12 3 2">
+        <button className="btn btn-primary"><i className="fa fa-plus"></i></button>
+      </Grid>
+    </div>
+  </div>
+)
