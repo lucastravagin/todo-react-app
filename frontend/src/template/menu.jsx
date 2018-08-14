3f813d246c99d3a3ebb5226ce0052da4592165af
Lucas <lucas_travagin1112@hotmail.com>
2018-08-14 11:28:36 -0300

    Criando o menu e confiugurando as rotas da aplicação

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
 export default props => (
-    <nav className="navbar navbar-inverse bg-inverse">
-        <div className="container">
-            <div class="navbar-header">
-                <a className="navbar-brand" href="#">
-                    <i className="fa fa-calendar-check-o"></i> TodoApp
-                </a>
-            </div>
-            <div id="navbar" className="navbar-collapse collapse">
-                <ul className="nav navbar-nav"> 
-                    <li><a href="#/todos">Tarefas</a></li>
-                    <li><a href="#/about">Sobre</a></li>
-                </ul>
-            </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#"></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#/todos">Todo <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#/about">Sobre</a>
+                </li>
+            </ul>
         </div>
     </nav>
 )
